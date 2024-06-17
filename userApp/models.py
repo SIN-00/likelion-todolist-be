d06857fd314992b825a24f5f60845c5fb3029f6c
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-06-17 17:48:35 +0900

    feat : 모델 생성

--- userApp/models.py
+++ userApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(modes.Model) :
+    username = models.CharField(
+        max_length = 150,
+        unique=True
+    )
+    password = models.CharField(
+        max_length = 150,
+    )
+def __str__(self): #유저클래스를 문자열로 바꿔야할 있을때
+    return self.username #유저클래스를 대표하는 문자열 
