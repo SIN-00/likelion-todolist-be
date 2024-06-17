39056124b1b35a3d72e9fc18d17eb9b44721dfb1
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-06-17 17:48:10 +0900

    feat : 유알엘 계층화

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -18,7 +18,4 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/', include(userApp.urls)), 
-    path('api/todos/', include(todoApp.urls)), 
 ]
