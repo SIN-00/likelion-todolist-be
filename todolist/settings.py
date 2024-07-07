bce0ee4d1979a19a86b66bcf8918ee505d71497e
SHIN__0.0 <ajtwoddltom@gmail.com>
2024-07-08 04:22:50 +0900

    feat : cors

--- todolist/settings.py
+++ todolist/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     "todoApp",
     "userApp",
     "rest_framework",
+    "corsheaders",
     ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,7 +53,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = "todolist.urls"
 
 TEMPLATES = [
